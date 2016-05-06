8e9a0ebe30a8594d98467e85d426922d7377ba41
rignaneseleo <rignanese.leo@outlook.com>
2016-05-06 19:48:28 +0200

    the strings.xml generated are now formatted

--- downloadXml.php
+++ downloadXml.php
@@ -14,40 +14,48 @@
 			header('Content-Disposition: attachment; filename="strings.xml"');
 			
 			//print credits
-			echo '<?xml version="1.0" encoding="utf-8"?>';
-			echo '<!-- **********************************************************  -->';
-			echo '<!-- THIS TRANSLATION IS MADE BY '.$_POST['username'].' USING StringsXmlBuilder -->';
-			echo '<!-- StringsXmlBuilder is an OPEN SOURCE tool realized by rignaneseleo -->';
-			echo '<!-- https://github.com/rignaneseleo/Strings.xml-builder-and-translator-PHP -->';
-			echo '<!-- **********************************************************  -->';
-			
-			
-			echo '<resources>';			
+			echo '<?xml version="1.0" encoding="utf-8"?>
+
+';			
+			echo '<!-- **********************************************************  -->
+';			
+			echo '<!-- THIS TRANSLATION IS MADE BY '.$_POST['username'].' USING StringsXmlBuilder -->
+';
+			echo '<!-- StringsXmlBuilder is an OPEN SOURCE tool realized by rignaneseleo <rignanese.leo@gmail.com>-->
+';
+			echo '<!-- https://github.com/rignaneseleo/Strings.xml-builder-and-translator-PHP -->
+';
+			echo '<!-- **********************************************************  -->
+
+';						
+			echo '<resources>
+';			
 			foreach($_POST['translatedValueIds'] as $key=>$value)
 			{
 				if($value!=""){
 					$value=str_replace("'", "\'", $value);//add the \'				
-					echo '<string name="'.$key.'">'.$value.'</string>';//create a <string> with its value
+					echo '<string name="'.$key.'">'.$value.'</string>
+';//create a <string> with its value
 				}				
 			}
 			echo '</resources>';
 			/*
 				$resources = new SimpleXMLElement('<resources/>');//create an xml file with the tag <resources>
-				
-				$resources->addChild('!-- TRANSLATION IS MADE BY '.$_POST['username'].'  using  Strings.xml Builder --><rignaneseleo');
-				
-				foreach($_POST[translatedValueIds] as   $key=>$value)
-				{
-				$value=str_replace("'", "\'", $value);//add the \'
-				
-				$string = $resources->addChild('string',$value);//create a <string> with its value
-				$string->addAttribute("name",$key);//set the "name" at the <string>
-				}
-				
-				$xml =$resources->asXML();
-				header('Content-Type: application/xml;');		
-				header('Content-Disposition: attachment; filename="strings.xml"');
+			
+			$resources->addChild('!-- TRANSLATION IS MADE BY '.$_POST['username'].'  using  Strings.xml Builder --><rignaneseleo');
+			
+			foreach($_POST[translatedValueIds] as   $key=>$value)
+			{
+			$value=str_replace("'", "\'", $value);//add the \'
+			
+			$string = $resources->addChild('string',$value);//create a <string> with its value
+			$string->addAttribute("name",$key);//set the "name" at the <string>
+			}
+			
+			$xml =$resources->asXML();
+			header('Content-Type: application/xml;');		
+			header('Content-Disposition: attachment; filename="strings.xml"');
 			echo $xml;*/
-		}
-	}
-?>
+			}
+			}
+			?>			
